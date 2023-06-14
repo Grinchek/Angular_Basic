903e4b9f96ea3e8f5f1883a1565b5c32eb9dd35a
Grinchik <warning686@gmail.com>
2023-06-14 18:34:22 +0300

    Pass the second lesson of Angular basic course

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
+export interface Role {
+  role: string,
+  text: string
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Angular_Basic';
+  title: string = 'Angular_Basic';
+  isParagraphavisible: boolean = true;
+  list: string[] = [
+    'List one',
+    'List two'
+  ];
+  roles: Role[] = [
+    {
+      role: 'Admin',
+      text: 'Admin'
+    },
+    {
+      role: 'User',
+      text: 'user'
+    },
+    {
+      role: 'Coctumer',
+      text: 'Ivan'
+    },
+    {
+      role: 'User',
+      text: 'user2'
+    },
+  ];
+  show() {
+    console.log('isParagraphavisible', this.isParagraphavisible)
+  }
+  getColor(user: Role): string {
+    return user.role === 'Admin' ? 'green' : user.role === 'User' ? 'blue' : 'red'
+  }
+  getDate() {
+    return new Date();
+  }
+
 }
